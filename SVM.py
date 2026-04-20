6e3aafc7e9eb1ba78831cb2abf6f4b0255b47c36
the-siddhartha14 <siddharthagautam414@gmail.com>
2026-04-20 11:30:24 +0530

    SVM.py

---
+++ SVM.py
@@ -0,0 +1,21 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+X = iris.data   # features
+y = iris.target # labels
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = SVC(kernel='linear')
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
